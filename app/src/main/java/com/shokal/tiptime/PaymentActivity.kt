a222c06a7f14cfe8f6c717cce1ab26467e787477
Raian Ibn Faiz <raian.faiz@bjitacademy.com>
2022-11-25 17:05:23 +0600

    updated some errors

--- app/src/main/java/com/shokal/tiptime/PaymentActivity.kt
+++ app/src/main/java/com/shokal/tiptime/PaymentActivity.kt
@@ -3,6 +3,7 @@ package com.shokal.tiptime
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.shokal.tiptime.databinding.ActivityPaymentBinding
 
 class PaymentActivity : AppCompatActivity() {
